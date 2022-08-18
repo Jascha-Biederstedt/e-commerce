a317e4a2d4a45adcc2865d6e552dfe04668029a3
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-08-18 10:14:31 +0200

    Add button to admin page to create new product

--- pages/admin.js
+++ pages/admin.js
@@ -1,5 +1,6 @@
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const Admin = () => {
   const router = useRouter();
@@ -25,6 +26,12 @@ const Admin = () => {
   return (
     <div className='text-center'>
       <h1 className='mt-10 font-extrabold text-2xl mb-8'>Admin</h1>
+
+      <Link href='/admin/new'>
+        <a className='inline mx-auto bg-black text-white px-3 py-1 text-lg'>
+          Add a new product
+        </a>
+      </Link>
     </div>
   );
 };
