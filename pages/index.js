73b215d3f7fc7a1fff083fbe6d0216c139784d6c
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-08-19 10:01:00 +0200

    Style the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 
 import prisma from 'lib/prisma';
 import { getProducts } from 'lib/data.js';
@@ -23,12 +24,21 @@ const Home = ({ products }) => {
       </Head>
 
       <div className='text-center '>
-        <h1 className='mt-10 font-extrabold text-2xl'>Shop</h1>
+        <h1 className='mt-10 font-extrabold text-4xl text-center'>Shop</h1>
 
-        <div className='mt-20 mx-auto max-w-sm'>
+        <div className='mt-20 sm:mx-auto max-w-4xl mx-10'>
           {products.map(product => (
-            <div className='mb-4 border border-black' key={product.id}>
-              {product.title} (${product.price / 100})
+            <div className='mb-4 grid sm:grid-cols-2' key={product.id}>
+              <div>
+                <Image src={`/` + product.image} width={'600'} height={'600'} />
+              </div>
+              <div className='sm:ml-10 mb-20 sm:mb-0'>
+                <h2 className='text-3xl font-extrabold'>{product.title}</h2>
+                <h3 className='text-2xl font-extrabold mb-4'>
+                  ${product.price / 100}
+                </h3>
+                <p className='text-xl'>{product.description}</p>
+              </div>
             </div>
           ))}
         </div>
