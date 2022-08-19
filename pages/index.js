059da75cf9abfc442cec484680ff25096780ee8f
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-08-19 09:59:37 +0200

    Load products to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 import Head from 'next/head';
 
-const Home = () => {
+import prisma from 'lib/prisma';
+import { getProducts } from 'lib/data.js';
+
+const getServerSideProps = async context => {
+  const products = await getProducts(prisma);
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
+
+const Home = ({ products }) => {
   return (
     <div>
       <Head>
@@ -11,6 +24,14 @@ const Home = () => {
 
       <div className='text-center '>
         <h1 className='mt-10 font-extrabold text-2xl'>Shop</h1>
+
+        <div className='mt-20 mx-auto max-w-sm'>
+          {products.map(product => (
+            <div className='mb-4 border border-black' key={product.id}>
+              {product.title} (${product.price / 100})
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
